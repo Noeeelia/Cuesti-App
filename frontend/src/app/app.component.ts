3ddfceb799de743080cebcf9b7b859c333088146
Noelia <noeerivaas@gmail.com>
2025-05-15 10:45:54 -0300

    Iniciando proyecto remoto

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { ImageModule } from 'primeng/image';
+import {StyleClassModule} from 'primeng/styleclass';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, 
+    ConfirmDialogModule,
+    ToastModule,
+    ButtonModule,
+    ImageModule,
+    StyleClassModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  providers: [ ConfirmationService, MessageService],
+})
+export class AppComponent {
+  title = 'frontend';
+}
