e815e9fb200b64e3db145cbd759e05a3d896e226
Noelia <noeerivaas@gmail.com>
2025-05-16 14:09:33 -0300

    Reubicación del home

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CrearEncuestaComponent } from './components/crearEncuesta/creaEncuesta.component';
 
 export const routes: Routes = [
-    // {
-//       path: '', 
-//   component: ComienzoComponent
-// },
-// {
-//     path: '**', 
-//     redirectTo: ''     
-// }
+    {
+        path: '', 
+            component: HomeComponent,
+    },
+    {
+        path: 'crearEncuesta',
+            component: CrearEncuestaComponent,
+    },
+    {
+        path: '**', 
+            redirectTo: ''     
+    }
 ];
